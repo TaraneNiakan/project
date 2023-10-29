bae5209642f757f1599cc7fb9cad5c7c2e56b6f6
arefeh Shahdadi <100937412+TaraneNiakan@users.noreply.github.com>
2023-10-29 22:40:53 +0330

    Add files via upload

---
+++ mini/mini-project1/javas/Script.js
@@ -0,0 +1,71 @@
+var number=20;
+
+var number2=40;
+
+var Variable=23 + "hello";
+
+var Variable2= 50 - 20 + "hello";
+
+var variable3="hello"+ 50+20;
+
+var person={FirstName:"tarane", age:19};
+
+//document.write(person.FirstName);
+
+//--------------//
+
+
+var isEqual;
+isEqual=(number==number2);
+//console.log(isEqual);
+
+//------------------//
+
+var colors=["red","blue",23];
+
+
+var colors=new Array("red","blue");
+
+//document.write(colors[1]);
+colors.reverse;
+colors.shift;
+colors.unshift;
+colors.pop;
+colors.push;
+
+var NewColors=colors.slice();
+
+var result=colors.indexOf("blue",2);
+
+
+var arrayString=colors.join();
+
+
+//---------------------------//
+
+if(
+    condition
+)
+{
+    //function
+}
+
+
+//------------//
+function myfunction(){
+var Variable2="variable2";
+
+}
+const Result2="mohammad";
+
+//console.log(Result2);
+
+//objects//
+
+var person={
+    firstName:"mmm",
+    llastName:"nn"
+
+}
+var person2=new Object();
+person2.phoneNumber=12345;
---
+++ mini/mini-project1/javas/bootcamp2/scrript.js
@@ -0,0 +1,102 @@
+/*let user ={
+    name: "tst",
+    age: "26",
+}
+function func(value){
+
+ console.log(user[value]);
+
+}
+func('age');*/
+
+/*let style ={
+    "background-color": "red",
+};
+
+function func (value){
+    console.log(style[value]);
+    
+
+}
+ func('background-color');
+*/
+/*let user ={
+    name:"tarane"
+}
+
+
+
+Object.assign(user, {name:"tst"},{age:"26"})
+console.log(user['age'])*/
+
+/*function bootcamp(name,duration){
+    return { 
+
+    name,
+    duration,
+    setFullInfo(){
+        console.log(this.name + this.duration)
+    }
+}
+
+}
+const cloudBootcamp= bootcamp('aa','2');
+// log object
+console.log(cloudBootcamp);
+// log value
+cloudBootcamp.setFullInfo();*/
+/*const data={
+name :"a",
+setFullInfo : ()=> {
+    return this.name + this.duration;
+}
+}
+console.log(this)*/
+
+//let data =[
+    
+/*{id : 1, name:"tese1"},
+{id: 2 , name: "test2"},
+
+]
+
+//data.forEach((element)=>{
+    
+
+new Set();
+const num1=[1,1,1,2,3,3,4,5,5,5]
+const num2=new Set(num1);
+for(const n of num2){
+console.log(n);}*/
+
+/*class fee{
+
+constructor(cost,discount){
+    this.cost=cost
+    this.discount=discount
+}
+
+amount(){
+return(cost-this.ddiscount)
+}
+
+ddiscount(){
+return((cost*this.discount)/100)
+
+}
+
+
+}
+const1 fee1=new fee(1000,20)
+const2 fee2=new fee(5000,30)*/
+
+function showlist(
+ 
+    list =document.getElementbyId("text").value,
+    
+)
+
+
+
+
+
